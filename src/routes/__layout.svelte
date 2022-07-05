5dfb1c9ae6fddfb621517788ef3abd844e961797
Ido Schacham <ido.schacham@wunderdog.fi>
2022-07-05 12:01:19 +0200

    Added layout and Svelte Material UI

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import Button, { Label, Icon } from '@smui/button';
+</script>
+
+<TopAppBar variant="static" color="secondary">
+	<Row>
+		<Section>
+			<Title>Klepto</Title>
+		</Section>
+		<Section align="end">
+			<Button variant="raised">
+				<Label>New Idea</Label>
+				<Icon class="material-icons">lightbulb</Icon>
+			</Button>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<main>
+	<slot />
+</main>
+
+<style>
+</style>
