4c8e9d29a740d3876908e37c49dc27870aee1cab
Ido Schacham <ido.schacham@wunderdog.fi>
2022-07-19 09:14:51 +0200

    Add error handling to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,46 @@
 import clientPromise from '../lib/mongodb-client';
 
 export async function get() {
-	const dbConnection = await clientPromise;
-	const db = dbConnection.db('klepto');
-	const pipeline = [
-		{
-			$lookup: {
-				from: 'users',
-				localField: 'userId',
-				foreignField: '_id',
-				as: 'user'
-			}
-		},
-		{
-			$set: {
-				user: { $arrayElemAt: ['$user', 0] }
-			}
-		},
-		{
-			$project: {
-				user: {
-					password: 0
+	try {
+		const dbConnection = await clientPromise;
+		const db = dbConnection.db('klepto');
+		const pipeline = [
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'userId',
+					foreignField: '_id',
+					as: 'user'
+				}
+			},
+			{
+				$set: {
+					user: { $arrayElemAt: ['$user', 0] }
+				}
+			},
+			{
+				$project: {
+					user: {
+						password: 0
+					}
 				}
 			}
-		}
-	];
+		];
 
-	const ideas = await db.collection('ideas').aggregate(pipeline).sort({ date: -1 }).toArray();
+		const ideas = await db.collection('ideas').aggregate(pipeline).sort({ date: -1 }).toArray();
 
-	return {
-		status: 200,
-		body: {
-			ideas
-		}
-	};
+		return {
+			status: 200,
+			body: {
+				ideas
+			}
+		};
+	} catch (error) {
+		return {
+			status: 400,
+			body: {
+				error
+			}
+		};
+	}
 }
