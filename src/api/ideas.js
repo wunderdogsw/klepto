6074ce850bf7a8cea18a7a86c308ab9af26fc12f
Ido Schacham <ido.schacham@wunderdog.fi>
2022-07-26 11:15:05 +0200

    Refactored deleting idea

--- src/api/ideas.js
+++ src/api/ideas.js
@@ -4,7 +4,7 @@ export const add = async (idea) => await fetchJson('/api/ideas/new', idea);
 
 export const edit = async (idea) => await fetchJson('/api/ideas/edit', idea);
 
-export const del = async (id) => await fetchJson(`/delete/${id}`, {}, 'DELETE');
+export const del = async (_id) => await fetchJson(`/api/ideas/delete`, { _id });
 
 export const vote = async (ideaId, isUp) =>
 	await fetchJson('/api/ideas/vote', { ideaId, isUp }, 'PATCH');
