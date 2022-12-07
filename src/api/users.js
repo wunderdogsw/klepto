c19f310dc7533c1ef9e32ffe57e39a4b8502359d
Ido Schacham <ido.schacham@wunderdog.fi>
2022-12-07 10:49:14 +0100

    bugfix: reload home when user logs in/out

--- src/api/users.js
+++ src/api/users.js
@@ -1,13 +1,16 @@
-import { goto } from '$app/navigation';
-
 import { fetchJson } from '$lib/utils';
 
 export const signup = (name, email, password) =>
 	fetchJson('/api/users/signup', { name, email, password });
 
-export const login = async (email, password) => fetchJson('/api/users/login', { email, password });
+const goHome = () => (location.href = '/');
+
+export const login = async (email, password) => {
+	await fetchJson('/api/users/login', { email, password });
+	goHome();
+};
 
 export const logout = async () => {
 	await fetchJson('/api/users/logout');
-	await goto('/');
+	goHome();
 };
